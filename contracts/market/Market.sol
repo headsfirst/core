1ae27c5b8ee38630525f20f58619af571347d70e
Marco Argentieri <marco@vulpem.com>
2018-02-28 18:14:01 +0100

    CurationMarket WIP && Added working minting and attempt of unminting, doesn't work

--- contracts/market/Market.sol
+++ contracts/market/Market.sol
@@ -1,43 +1,90 @@
 pragma solidity ^0.4.18;
 
+import "zeppelin-solidity/contracts/token/MintableToken.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
 import "../tokens/SingularityNetToken.sol";
 
+
+contract SpikeToken is MintableToken {
+    string public constant name = "Spike Token";
+    string public constant symbol = "SPIKE"; 
+    uint8 public constant decimals = 0;
+
+    event Unmint(address indexed beneficiary, uint256 amount);
+
+    function unmint(address beneficiary, uint256 amount) onlyOwner canMint public returns (bool) {
+        totalSupply = totalSupply.sub(amount);
+        balances[beneficiary] = balances[beneficiary].sub(amount);
+
+        Unmint(beneficiary, amount);
+        return true;
+    } 
+}
 
 contract Market {
     using SafeMath
     for uint256;
 
+    uint256 public constant AGI_BASE_UNIT = 1;
+    uint256 public constant SPIKE_BASE_UNIT = 1;
+    
+
+    MintableToken public spike;
     SingularityNetToken public token;
+
     address public owner;
     // amount of AGI at stake
     uint256 public pool = 0;
-    // supply of MintedToken 
-    uint256 public totalSupply = 0;
     // next price multiplier in AGI for minting next token
     uint256 public nextPrice = 1 * (10 ** 8);
 
-    //Balances of MintedToken
-    mapping(address => uint256) public balances;
 
     function Market(address _token) public {
         owner = msg.sender;
+        spike = new SpikeToken();
         token = SingularityNetToken(_token);
     }
 
-    function ask() public {
+    function join() public {
         // We want to add 1 AGI. Use cogs instead?
         require(token.transferFrom(msg.sender, address(this), nextPrice));
+        // Update pool 
         pool = pool.add(nextPrice);
-        //Minting 
-        uint256 mintedToken = 1;
-        balances[msg.sender] = mintedToken;
-        totalSupply = totalSupply.add(mintedToken);
+        //Minting 1 SPIKE token
+        spike.mint(msg.sender, SPIKE_BASE_UNIT);
         //Update the rate 
-        nextPrice = nextPrice.add(1 * (10 ** 8));
+        require(updatePrice(AGI_BASE_UNIT,true));
     }
 
-    function claimFunds() {
+    function quit() public {
+        require(spike.balanceOf(msg.sender) > 0);
+        //Calculate the available reward 
+        uint256 reward = pool.div(spike.totalSupply());
+
+        //Now we can transfer back his AGI
+        require(token.transfer(msg.sender, reward));
+        //Update pool
+        pool = pool.sub(reward);
+        // Update now the SPIKE total supply along with the balance
+        //spike.unmint(msg.sender, SPIKE_BASE_UNIT);
+        //Update price in AGI
+        require(updatePrice(AGI_BASE_UNIT, false));
+    }
+
+    function updatePrice(uint256 amount, bool action) internal returns (bool) {
+        require(amount > 0);
+
+        if (action) {
+            nextPrice = nextPrice.add(amount * (10 ** 8));
+        } else {
+            nextPrice = nextPrice.sub(amount * (10 ** 8));            
+        }
+        return true;
+    }
+
+   
+
+    function claimFunds() public {
         require(owner == msg.sender);
         token.transfer(owner, token.balanceOf(address(this)));
     }
